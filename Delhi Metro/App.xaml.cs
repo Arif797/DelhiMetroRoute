7a6975c5472b82f47f200bbb10dc23e9859d6010
arifahmad4299 <arif.ahmad@successive.tech>
2020-06-18 22:58:58 +0530

    Delhi Metro Files

---
+++ Delhi Metro/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DelhiMetro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage())
+            {
+                BarBackgroundColor = Color.FromHex("#016bb7"),
+                BarTextColor = Color.White,
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
